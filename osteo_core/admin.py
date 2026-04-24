40dbf8e9c7e320eda32cdd48aacf0fe61562bd0c
Cody G. <cgunter13@ivytech.edu>
2026-04-24 19:50:30 -0400

    Assessment form HTML and added assessment to the admin panel.

--- osteo_core/admin.py
+++ osteo_core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Client, Horse, Appointment
+from .models import Client, Horse, Appointment, Assessment
 
 admin.site.register(Client)
 admin.site.register(Horse)
 admin.site.register(Appointment)
+admin.site.register(Assessment)
